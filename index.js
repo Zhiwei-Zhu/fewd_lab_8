90d7996503674488719d680553e3c610a78ecc40
Vincent Zhu <zhu.zhiwei.vincent@gmail.com>
2020-11-14 20:21:28 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let Planets = ["Mars","earth","Saturn","Uranus","Mercure"];
+
+for (let i=0;i<5;i++){
+    console.log(Planets[i])
+}
+
+var hello = function () {
+    return  'Hello world!';
+};
+
+console.log(hello());
+
+
